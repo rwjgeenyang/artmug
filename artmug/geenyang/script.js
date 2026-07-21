50d9cbb077767ed6a516dc23ee04cd51d28d03eb
rwjgeenyang <86219845+rwjgeenyang@users.noreply.github.com>
2026-07-21 17:02:39 +0900

    Update script.js

--- artmug/geenyang/script.js
+++ artmug/geenyang/script.js
@@ -1,20 +1,31 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbzswPe02rjw2YqT_Uh4eqBUujwEaSYdL7jvb_9WBq-S0vAb9xW4IW4SkkkmdD6eoupG/exec";
 
-async function loadData() {
+async function loadData(){
+
     const response = await fetch(API_URL);
     const data = await response.json();
 
-    document.getElementById("app").textContent = data.text;
+    document.getElementById("title").textContent = data.title;
+
+    document.getElementById("subtitle").textContent = data.subtitle;
+
+    document.getElementById("description").textContent = data.description;
+
+    document.getElementById("price").textContent =
+        Number(data.price).toLocaleString() + "원";
 
     resize();
+
 }
 
-function resize() {
+function resize(){
+
     parent.postMessage({
-        type: "resize",
-        height: document.documentElement.scrollHeight
-    }, "*");
+        type:"resize",
+        height:document.documentElement.scrollHeight
+    },"*");
+
 }
 
 window.onload = loadData;
