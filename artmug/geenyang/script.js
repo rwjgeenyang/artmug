c115d344bdf5f5709f5a7f461a3e0d4b6b86757e
rwjgeenyang <86219845+rwjgeenyang@users.noreply.github.com>
2026-07-21 16:44:17 +0900

    Update script.js

--- artmug/geenyang/script.js
+++ artmug/geenyang/script.js
@@ -1,1 +1,9 @@
+function resize(){
+    parent.postMessage({
+        type:"resize",
+        height:document.documentElement.scrollHeight
+    }, "*");
+}
 
+window.onload = resize;
+window.onresize = resize;
