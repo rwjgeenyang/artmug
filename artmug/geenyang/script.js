860205513bcf465ee9bebf5270f97a80c6e22697
rwjgeenyang <86219845+rwjgeenyang@users.noreply.github.com>
2026-07-21 16:47:06 +0900

    Update script.js

--- artmug/geenyang/script.js
+++ artmug/geenyang/script.js
@@ -1,9 +1,21 @@
-function resize(){
+const API_URL =
+"https://script.google.com/macros/s/AKfycbzswPe02rjw2YqT_Uh4eqBUujwEaSYdL7jvb_9WBq-S0vAb9xW4IW4SkkkmdD6eoupG/exec";
+
+async function loadData() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    document.getElementById("app").textContent = data.text;
+
+    resize();
+}
+
+function resize() {
     parent.postMessage({
-        type:"resize",
-        height:document.documentElement.scrollHeight
+        type: "resize",
+        height: document.documentElement.scrollHeight
     }, "*");
 }
 
-window.onload = resize;
+window.onload = loadData;
 window.onresize = resize;
